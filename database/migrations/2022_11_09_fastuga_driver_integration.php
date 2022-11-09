d66d5f92cb79650c6139d281003013cd8c473370
Ricardo Franco <ricardosf195@gmail.com>
2022-11-09 11:45:18 +0000

    fastuga_driver migration created

---
+++ database/migrations/2022_11_09_fastuga_driver_integration.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('drivers', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('phone', 20);
+            $table->string('license_plate', 9);
+
+            $table->json('custom')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('orders_driver_delivery', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('order_id')->unsigned();
+            $table->foreign('order_id')->references('id')->on('orders');
+
+            $table->string('delivery_location');
+
+            $table->integer('tax_fee');
+
+            $table->timestamp('delivery_started_at');
+            $table->timestamp('delivery_ended_at');
+
+            $table->json('custom')->nullable();
+            $table->unique(['order_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('drivers');
+        Schema::dropIfExists('orders_driver_delivery');
+    }
+};
