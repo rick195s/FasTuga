c4e54dd460441257d49850fce13377d949f29924
marco2201708 <2201708@my.ipleiria.pt>
2022-12-04 18:46:44 +0000

    Devolver encomendas por entregar

--- app/Http/Controllers/api/OrdersDeliveryController.php
+++ app/Http/Controllers/api/OrdersDeliveryController.php
@@ -15,13 +15,60 @@ class OrdersDeliveryController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        /*$ordersToDrive = OrderDriverDelivery::select("order_id")->get();
-        $orders = Order::whereIn("id",$ordersToDrive)->paginate(10);//Adicionar cáusula where (delivery_start_date == null)
-        return $orders;*/
+        $ordersToCollection = OrderDriverDelivery::whereNull("delivery_started_at")->paginate(10);
 
-        return OrderDriverDeliveryResource::collection(OrderDriverDelivery::paginate(10));
+        /*foreach ($ordersToCollection as $order) {
+            BingWebSearch($order->delivery_location)
+            if(){
+
+            }
+        }*/
+       /* if($request->filter == 'max.5km'){
+            //get destination coordinates
+            //get user coordinates
+
+            return ["5km"];
+        }
+        if($request->filter == 'max.10km'){
+            return ["10km"];
+        }
+        if($request->filter == 'max.15km'){
+            return ["15km"];
+        }*/
+        return OrderDriverDeliveryResource::collection($ordersToCollection);
+
+
+    }
+
+    function BingWebSearch ($query) {
+        $url = .env('API_URL')
+        $key = .env('API_KEY')
+        
+        /* Prepare the HTTP request.
+         * NOTE: Use the key 'http' even if you are making an HTTPS request.
+         * See: http://php.net/manual/en/function.stream-context-create.php.
+         */
+        $headers = "Ocp-Apim-Subscription-Key: $key\r\n";
+        $options = array ('http' => array (
+                              'header' => $headers,
+                               'method' => 'GET'));
+    
+        // Perform the request and get a JSON response.
+        $context = stream_context_create($options);
+        $result = file_get_contents($url . "?q=" . urlencode($query), false, $context);
+    
+        // Extract Bing HTTP headers.
+        $headers = array();
+        foreach ($http_response_header as $k => $v) {
+            $h = explode(":", $v, 2);
+            if (isset($h[1]))
+                if (preg_match("/^BingAPIs-/", $h[0]) || preg_match("/^X-MSEdge-/", $h[0]))
+                    $headers[trim($h[0])] = trim($h[1]);
+        }
+    
+        return array($headers, $result);
     }
 
     /**
