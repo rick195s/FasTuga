92277e357c00611247d073007747a376056f6d5a
marco2201708 <2201708@my.ipleiria.pt>
2022-12-05 00:29:03 +0000

    final filters

--- app/Http/Controllers/api/OrdersDeliveryController.php
+++ app/Http/Controllers/api/OrdersDeliveryController.php
@@ -8,9 +8,13 @@ use App\Models\OrderDriverDelivery;
 use App\Models\Order;
 use App\Http\Resources\OrderDriverDeliveryResource;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class OrdersDeliveryController extends Controller
 {
+
      /**
      * Display a listing of the resource.
      *
@@ -19,39 +23,66 @@ class OrdersDeliveryController extends Controller
     public function index(Request $request)
     {
         $ordersToDeliver = OrderDriverDelivery::whereNull("delivery_started_at")->paginate(10);
-
-        if($request->filter == 'max.5km'){
-            $ordersToDeliver = $this->selectOrdersByFilter($ordersToDeliver,5);
+        if($request->filter != 'All'){
+            $ordersToDeliver = $this->selectOrdersByFilter($ordersToDeliver,$request->filter);
+            $ordersToDeliver = $this->paginate($ordersToDeliver);
         }
-        if($request->filter == 'max.10km'){
-            $ordersToDeliver = $this->selectOrdersByFilter($ordersToDeliver,10);
-        }
-        if($request->filter == 'max.15km'){
-            $ordersToDeliver = $this->selectOrdersByFilter($ordersToDeliver,15);
-        }
-        return $ordersToDeliver;
+        return OrderDriverDeliveryResource::collection($ordersToDeliver);
 
 
+    }
+
+    public function paginate($items, $perPage = 5, $page = null, $options = [])
+    {
+        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+        $items = $items instanceof Collection ? $items : Collection::make($items);
+        return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
     }
 
     private function selectOrdersByFilter($ordersToDeliver, $filter){
         $ordersInFilter = array();
         $latitude = null;
         $longitude = null;
+        $latitudeFastuga = 39.734730;
+        $longitudeFastuga = -8.820921;
+        
         foreach ($ordersToDeliver as $order) {
             $response = Http::get('http://api.positionstack.com/v1/forward?access_key=ce376ccadaa61d0f359a19b28d856659&query='.$order->delivery_location);
             if($response->object()->data != null){
                 $latitude = $response->object()->data[0]->latitude;
                 $longitude = $response->object()->data[0]->longitude;
-                //Calculate distance with coordinates
                 //SE order <= filtro adicionar ELSE break 
-                array_push($ordersInFilter,$order);
-            }else {
+                if($this->distance($latitude, $longitude, $latitudeFastuga, $longitudeFastuga, "K") <= $filter){
+                    array_push($ordersInFilter,$order);
+                }
+            }/*else {
                 array_push($ordersInFilter,"Failed to fing coordinates");
-            }
+            }*/
         }
         return $ordersInFilter;
     }
+
+    function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+        if (($lat1 == $lat2) && ($lon1 == $lon2)) {
+          return 0;
+        }
+        else {
+          $theta = $lon1 - $lon2;
+          $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+          $dist = acos($dist);
+          $dist = rad2deg($dist);
+          $miles = $dist * 60 * 1.1515;
+          $unit = strtoupper($unit);
+      
+          if ($unit == "K") {
+            return ($miles * 1.609344);
+          } else if ($unit == "N") {
+            return ($miles * 0.8684);
+          } else {
+            return $miles;
+          }
+        }
+      }
 
     /**
      * Store a newly created resource in storage.
